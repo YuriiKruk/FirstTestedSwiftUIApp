54f49553b32b639976ac5866a1cab0b6bdd90743
Yurii Kruk <pmyurk@gmail.com>
2021-07-14 18:26:17 +0300

    moved ColorUIMagazzzine to separate file

--- MAGAZZZINE/AppView.swift
+++ MAGAZZZINE/AppView.swift
@@ -28,15 +28,7 @@ struct AppView: View {
     }
 }
 
-struct ColorUIMagazzine {
-    let loginBackground = UIColor(rgb: 0x212529)
-    let signIn = UIColor(rgb: 0xf9dbbd)
-    let signUp = UIColor(rgb: 0xecc8af)
-    let loginButton = UIColor(rgb: 0xfe5f55)
-    let loginTextColor = UIColor(rgb: 0x1d3557)
-    let backgroundColor = UIColor(rgb: 0x6d6875)
-    let buttonColor = UIColor(rgb: 0xe5989b)
-} 
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ MAGAZZZINE/ColorUIMagazzzine.swift
@@ -0,0 +1,18 @@
+//
+//  ColorUIMagazzzine.swift
+//  MAGAZZZINE
+//
+//  Created by Yury Kruk on 14.07.2021.
+//
+
+import SwiftUI
+
+struct ColorUIMagazzine {
+    let loginBackground = UIColor(rgb: 0x212529)
+    let signIn = UIColor(rgb: 0xf9dbbd)
+    let signUp = UIColor(rgb: 0xecc8af)
+    let loginButton = UIColor(rgb: 0xfe5f55)
+    let loginTextColor = UIColor(rgb: 0x1d3557)
+    let backgroundColor = UIColor(rgb: 0x6d6875)
+    let buttonColor = UIColor(rgb: 0xe5989b)
+}
