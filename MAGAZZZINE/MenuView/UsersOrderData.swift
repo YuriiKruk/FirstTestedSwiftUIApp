3344673bd8155c4c4d17d97c72e34644536a13cb
Yurii Kruk <pmyurk@gmail.com>
2021-07-14 03:53:50 +0300

    + moved users order data to separate file

--- MAGAZZZINE/MenuView/UsersOrderData.swift
+++ MAGAZZZINE/MenuView/UsersOrderData.swift
@@ -1,11 +1,18 @@
 //
-//  AssortmentData.swift
+//  UsersOrderData.swift
 //  MAGAZZZINE
 //
 //  Created by Yury Kruk on 14.07.2021.
 //
 
 import SwiftUI
+
+struct Shoes: Identifiable {
+    var id = UUID()
+    let name: String
+    let price: Int
+    let index: Int
+}
 
 struct Assortment {
     let shoes = [Shoes(name: "The Nike Air Max 1", price: 140, index: 0),
@@ -16,6 +23,13 @@ struct Assortment {
                  Shoes(name: "The Nike Air Max 95", price: 160, index: 5),
                  Shoes(name: "The Nike Cortez", price: 75, index: 6),
                  Shoes(name: "Tom Sachs x Nike Mars Yard", price: 3599, index: 7)]
+}
+
+struct Order: Codable, Identifiable {
+    var id = UUID()
+    var name: String
+    var price: Int
+    var size: String
 }
 
 class UsersOrder: ObservableObject {
@@ -36,4 +50,13 @@ class UsersOrder: ObservableObject {
             }
         }
     }
+}
+
+class Sale: ObservableObject {
+    @Published var score: Int = UserDefaults.standard.integer(forKey: "score") {
+        didSet {
+            UserDefaults.standard.set(self.score, forKey: "score")
+        }
+    }
+    
 }
